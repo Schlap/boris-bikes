7acc05f4b59e7e9f5d7e1c454d4e40de5ab4433d
Hadi <hadichalabi@gmail.com>
2014-09-15 18:48:25 +0100

    Added Fix Method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -11,10 +11,17 @@ it "should not be broken after we create it" do
 	#a method "broken?" that should return flase
 	expect(the_bike).not_to be_broken
 	end
-	
+
 it "should be able to break" do
 	bike = Bike.new
 	bike.break!
 	expect(bike).to be_broken
+	end
+
+it "should be able to get fixed" do
+	bike = Bike.new
+	bike.break!
+	bike.fix!
+	expect(bike).not_to be_broken
 	end
 end
